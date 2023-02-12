0fe4d994ec9e024f1765c3a77cb8ac3716085668
Alejandro5852 <3006215820101@ingenieria.usac.edu.gt>
2023-02-12 17:34:00 -0600

    [ADD] Feature calculadora

--- controllers/tarea1.js
+++ controllers/tarea1.js
@@ -3,4 +3,7 @@ const controller = {};
 controller.getInfo = (req, res) => {
     return res.status(200).json({ Nombre: 'César Alejandro Sosa Enríquez', Carnet: '201800555' });
 }
+controller.calculadora = (req, res) => {
+    return res.status(200).json({ resultado: req.body.num1 + req.body.num2 });
+}
 module.exports = controller;
