62d9450a6090844974bbffa6021039bfa193cdad
Alejandro5852 <3006215820101@ingenieria.usac.edu.gt>
2023-02-12 17:19:23 -0600

    [ADD] Feature info-carnet

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,4 @@
 const router = require('express').Router();
 const tareaController = require('../controllers/tarea1');
+router.get('/api/info-carnet-/201800555', tareaController.getInfo);
 module.exports = router;
